c14497247d38afca18a7335a2d8a787835c6d819
Denis Künzel <coding@kuenzelit.de>
2020-09-14 22:54:50 +0200

    Added cli info

--- getSystemInfo.php
+++ getSystemInfo.php
@@ -24,7 +24,13 @@ $token = $config['server']['token'];
 $api = new SysDashApi($config['server']['baseUrl'], $token, $config['server']['systemID']);
 
 if (isset($collectedData['software']))
+{
+    echo "Sending software info...";
     $api->sendSoftware($collectedData['software']);
+}
 
 if (isset($collectedData['os']))
+{
+    echo "Sending os info...";
     $api->sendOS($collectedData['os']);
+}
